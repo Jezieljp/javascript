185a97ac3f92e24224637ebe7ccb0dbf1cf7fbf5
jezieljp <cccccc157@hotmail.com>
2018-02-17 21:58:34 -0200

    ERRO NO GULP-NOTIFY

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var notify = require('gulp-notify');
+var browserSync = require("browser-sync").create();
+//var del = require("del");
+
+//LIMPANDO O CACHE
+/*gulp.task("cache:css", function(){
+    del("./course/css/style.css")
+});
+
+//LIMPANDO CACHE
+gulp.task("cache:js", function(){
+    del("./course/css/*.css")
+});*/
 
 //PRIMEIRA FORMA DE USA O BROWSER-SYNC
 gulp.task('server', function () {
@@ -10,11 +22,17 @@ gulp.task('server', function () {
     });
     gulp.watch("./course/css/bootstrap.min.css");
     gulp.watch("./course/js/**/*.js", ['js']);
-    gulp.watch('./course/index.htm', ['html']);
+    gulp.watch('./course/index.html', ['html']);
 });
 //TASK HTML
 gulp.task('html', function () {
     return gulp.src('./course/index.html')
+        .pipe(browserSync.stream());
+});
+//TASK CSS
+gulp.task('css', function () {
+    return gulp.src('./course/css/style.css')        
+        .on('error', notify.onError({title: "erro css", message: "<%= erro.message%>"}))
         .pipe(browserSync.stream());
 });
 //TASK JS
